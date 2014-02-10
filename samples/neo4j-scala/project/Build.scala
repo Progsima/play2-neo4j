59c41efa5d460a745ba90591690f39a86a2d5b38
sim51 <github@bsimard.com>
2014-02-10 19:49:24 +0100

    organize deps & adding json-schema lib

--- samples/neo4j-scala/project/Build.scala
+++ samples/neo4j-scala/project/Build.scala
@@ -8,18 +8,15 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Default Neo4j plugin deps
-    "org.neo4j.app" % "neo4j-server" % "2.0.0" classifier "static-web" classifier "" exclude("org.slf4j", "slf4j-jdk14"),
-    "com.wordnik" %% "swagger-play2" % "1.3.2" exclude("com.fasterxml.jackson.module","jackson-module-scala")
+    "com.wordnik" %% "swagger-play2" % "1.3.2",
+    "com.github.fge" % "json-schema-validator" % "2.1.7"
   )
 
   val play2neo4jModule = RootProject(file("../../module"))
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // after having running publish-local in the plugin using SBT, the jar will be put in the ivy2 repo
-    //   >> if so, uncomment the next line to set your local ivy2 repo as a new resolver
-    // resolvers += Resolver.file("Local ivy2 Repository", file(Path.userHome.absolutePath+"/.ivy2/local"))(Resolver.ivyStylePatterns)
+    resolvers += Resolver.url("fge-json-schema", url("http://dl.bintray.com/fge/maven"))(Resolver.mavenStylePatterns)
   ).dependsOn(play2neo4jModule).aggregate(play2neo4jModule)
 
 }
