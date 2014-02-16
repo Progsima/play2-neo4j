ec9f4e90bd91437f656fe718c7228f029d5ab29b
sim51 <github@bsimard.com>
2014-02-17 00:07:53 +0100

    rename package object

---
+++ module/app/com/logisima/play/neo4j/item/Evolution.scala
@@ -0,0 +1,13 @@
+package com.logisima.play.neo4j.item
+
+/**
+ * A cypher evolution.
+ *
+ * An evolution includes ‘up’ changes, to upgrade to to the version, as well as ‘down’ changes, to downgrade the database
+ * to the previous version.
+ *
+ * @param revision revision number
+ * @param cypher_up the cypher statements for UP application
+ * @param cypher_down the cypher statements for DOWN application
+ */
+case class Evolution(revision: Int, cypher_up: String = "", cypher_down: String = "", hash :String)
