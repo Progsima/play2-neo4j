3b0076368f8a643479d8077b79611634c613f968
sim51 <github@bsimard.com>
2014-03-10 22:58:07 +0100

    JS application

---
+++ samples/neo4j-scala/public/front/README.md
@@ -0,0 +1,15 @@
+# Frontend application
+
+## Installation
+
+* Installing node.js for npm. See https://github.com/joyent/node/blob/master/README.md
+* Launch  `npm update` this will download JS library for development (ex: gulp)
+    * npm install --save-dev -g gulp
+    * npm install --save-dev gulp
+    * npm install --save-dev gulp-jshint
+    * npm install --save-dev gulp-less
+    * npm install --save-dev gulp-concat
+    * npm install --save-dev gulp-livereload
+    * ...
+* Update application dependencies with bower : `bower install`
+
---
+++ samples/neo4j-scala/public/front/gulpfile.js
@@ -0,0 +1,69 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    minifyCSS = require('gulp-minify-css'),
+    exec = require('gulp-exec'),
+    bower = require('gulp-bower'),
+    livereload = require('gulp-livereload'),
+    watch = require('gulp-watch'),
+    concat = require('gulp-concat'),
+    less = require('gulp-less');
+
+
+/**
+ * Default task
+ */
+gulp.task("default", ["build"]);
+
+
+/**
+ * Update : Run bower update cmd.
+ */
+gulp.task("update", function(){
+    bower()
+        .pipe(gulp.dest('./app/lib/'));
+});
+
+/**
+ * Compile : JS Hint task
+ */
+gulp.task('jshint', function() {
+    gulp.src('./app/js/*.js')
+        .pipe(concat("main.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest('./app/js'))
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+/**
+ * Compile : LESS compilation
+ */
+gulp.task('less', function() {
+    gulp.src('./app/less/**/*.less')
+        .pipe(less())
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./app/css'))
+});
+
+/**
+ * Compile task.
+ */
+gulp.task('compile', ['jshint', 'less']);
+
+/**
+ * Build task  : update & compile
+ */
+gulp.task("build", ['update', 'compile']);
+
+/**
+ * Gulp watch : on each change file, run compile task
+ */
+gulp.task('watch', function() {
+    var server = livereload();
+    // watch change files into dest files
+    gulp.watch('./app/**').on('change', function(file) {
+        server.changed(file.path);
+        gulp.run('compile')
+    });
+});
