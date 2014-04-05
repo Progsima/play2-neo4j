dfb16cf2702f61aadd637a1eb7afdde247993830
sim51 <github@bsimard.com>
2014-04-05 15:30:26 +0200

    remove swagger

--- samples/neo4j-scala/app/controllers/ContentTypes.scala
+++ samples/neo4j-scala/app/controllers/ContentTypes.scala
@@ -15,17 +15,11 @@ import scala.concurrent.Future
  *
  * @author : bsimard
  */
-@Api(value = "/types", description = "Operations about content type")
 object ContentTypes extends Controller {
 
   /**
    * List all content type in the plateform.
    */
-  @ApiOperation(
-    value = "List content type",
-    notes = "Returns a list of content type",
-    httpMethod = "GET"
-  )
   def list(page :Int, row :Int, sort :String, order :String, filter :String) = Action.async { implicit request =>
 
     for ( (total, data) <- ContentType.list(page, row, sort, order, filter)) yield {
@@ -33,23 +27,13 @@ object ContentTypes extends Controller {
         .as("application/json")
         .withHeaders(("X-Total-Row" -> total.toString))
     }
+
   }
 
   /**
    * Get details of the specified content type.
    */
-  @ApiOperation(
-    value = "Get a content type by its name",
-    notes = "Returns a content type",
-    response = classOf[models.ContentType],
-    httpMethod = "GET"
-  )
-  @ApiResponses(Array(
-    new ApiResponse(code = 404, message = "Content Type not found")
-  ))
-  def get(
-           @ApiParam(value = "Name of the content type", required = true) @PathParam("name") name: String
-  ) = Action.async {
+  def get(name: String ) = Action.async {
 
     ContentType.get(name).map {
       case Some(contentType) => Ok(Json.toJson(contentType)).as("application/json")
@@ -63,20 +47,6 @@ object ContentTypes extends Controller {
    * This a POST because it's not safe & idempotent (there is a unique constraint on name).
    * If succeed, we return a 201 with object into the body
    */
-  @ApiOperation(
-    value = "Create a new content type",
-    notes = "Returns the content type created",
-    response = classOf[models.ContentType],
-    httpMethod = "POST"
-  )
-  @ApiImplicitParams(Array(
-    new ApiImplicitParam(value = "Content type that needs to be created", required = true, dataType = "ContentType", paramType = "body")
-  ))
-  @ApiResponses(Array(
-    new ApiResponse(code = 201, message = "Content Type created"),
-    new ApiResponse(code = 400, message = "Invalid body request"),
-    new ApiResponse(code = 409, message = "Constraint violation")
-  ))
   def create() = Action.async(parse.json) { implicit request =>
 
       JsonTools.validateSchema((request.body \ "schema").as[String]) match {
@@ -95,22 +65,9 @@ object ContentTypes extends Controller {
 
   /**
    * Update the specified content type.
+   * It's a PUT.
    */
-  @ApiOperation(
-    value = "Update a content type by its name",
-    notes = "Returns the content type updated",
-    response = classOf[models.ContentType],
-    httpMethod = "PUT"
-  )
-  @ApiImplicitParams(Array(
-    new ApiImplicitParam(value = "Content type that needs to be updates", required = true, dataType = "ContentType", paramType = "body")
-  ))
-  @ApiResponses(Array(
-    new ApiResponse(code = 400, message = "Invalid body request"),
-    new ApiResponse(code = 404, message = "Content Type not found"),
-    new ApiResponse(code = 409, message = "Constraint violation")
-  ))
-  def update(@ApiParam(value = "Name of the content type", required = true) @PathParam("name") name: String) = Action.async(parse.json) { implicit request =>
+  def update( name: String ) = Action.async(parse.json) { implicit request =>
 
       JsonTools.validateSchema((request.body \ "schema").as[String]) match {
         case Some(errors) => Future { BadRequest(Json.toJson(errors)) }
@@ -127,13 +84,7 @@ object ContentTypes extends Controller {
   /**
    * Delete the specified content type.
    */
-  @ApiOperation(
-    value = "Delete a content type by its name",
-    response = classOf[Void],
-    httpMethod = "DELETE")
-  @ApiResponses(Array(new ApiResponse(code = 204, message = "Success")))
-  def delete(@ApiParam(value = "Name of the content type", required = true) @PathParam("name") name: String) = Action.async(parse.empty) {
-    implicit request =>
+  def delete( name: String ) = Action.async(parse.empty) { implicit request =>
 
       for (isOk <- ContentType.delete(name)) yield {
         if (isOk) {
