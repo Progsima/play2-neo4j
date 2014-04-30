d3d605301c166145809d3d2ace8fe099d7fda662
Benoît Simard (@logisima) <contact@bsimard.com>
2014-04-30 18:15:58 +0200

    better json schema display error

--- samples/neo4j-scala/public/front/app/modules/lg-jsonschema/services/jsonschema-validation.js
+++ samples/neo4j-scala/public/front/app/modules/lg-jsonschema/services/jsonschema-validation.js
@@ -1,20 +1,52 @@
 /**
  * Factory for JSON Schema validation.
  */
-lgJsonschema.service('lgJsonSchemaValidation', [ function() {
+lgJsonschema.service('lgJsonSchemaValidation', [function() {
 
     /**
      * Validate data with the specify json schema.
      *
      * @param schema JSON schema
      * @param data object to validate
+     *
      * @return errors (if it's empty, it's good !).
      */
     this.validate = function (schema, data) {
         var result = tv4.validateMultiple(data, schema);
         var errors = [];
         if( !result.valid ) {
-            errors = result.errors.concat(result.missing);
+            for ( var i=0 ; i < result.errors.length ; i++ ) {
+                var error = result.errors[i];
+
+                var field;
+                if ( error.dataPath != null && error.dataPath.length > 0 ) {
+                    var code = "schema.properties";
+                    var path = error.dataPath.split('/');
+                    for ( var j=0 ; j < path.length ; j++ ) {
+                        if ( path[j] !=  "" ) {
+                            code += "['" + path[j] + "']";
+                        }
+                    }
+                    field = eval(code);
+                }
+                else {
+                    var path = error.schemaPath.split('/');
+                    var code = "schema";
+                    for ( var j=0 ; j < path.length ; j++ ) {
+                        if ( path[j] !=  "" ) {
+                            code += "['" + path[j] + "']";
+                        }
+                    }
+                    var name = eval(code);
+                    field = eval("schema.properties." + name);
+                }
+
+                errors.push({
+                   code : error.code,
+                   message : error.message,
+                   field : field
+                });
+            }
         }
         return errors;
     };
